fa3d97c248b7e0d198e68691a71b62e34b3e4e4d
pwnshell <65465491+pwnshell@users.noreply.github.com>
2020-05-19 17:23:05 +0530

    Update subdomain-scan.py

--- subdomain-scan.py
+++ subdomain-scan.py
@@ -1,34 +1,44 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
-import requests
+
+# By: pwnshell
+import pyfiglet
 import termcolor
+import optparse
 import os.path
+import requests
 
 try:
-    print termcolor.colored('====================================================', 'red', attrs=['bold'])
-    print termcolor.colored('   SUB DOMAIN SCANNER BY: pwnshell', 'green', attrs=['bold'])
-    print termcolor.colored('====================================================', 'red', attrs=['bold'])
-    target_url = raw_input(termcolor.colored('\nPlease enter Domain Name to scan: ', 'red', attrs=['bold']))
-    target_list = raw_input(termcolor.colored('\nPlease enter complete wordlist path e.g./usr/share/wordlist/file.txt: ', 'red', attrs=['bold']))
-    if os.path.exists(target_list) != True:
+
+    parser = optparse.OptionParser()
+    parser.add_option("-d", "--domain", dest="domain", help="Domain Name")
+    parser.add_option("-w", "--wordlist", dest="wordlist", help="Worlist File i.e. full path")
+    
+    (options, args) = parser.parse_args()
+
+    print termcolor.colored('                 Sub-Domain Scanner By:!!', 'red', attrs=['bold'])
+    banner = ascii_banner = pyfiglet.figlet_format("            pwnshell")
+    print termcolor.colored(banner, 'red', attrs=['bold'])
+
+    if os.path.exists(str(options.wordlist)) != True:
         print termcolor.colored('\nPlease enter a valid wordlist and re-run the program.', 'red', attrs=['bold'])
     else:    
-        print termcolor.colored('\nGO...GRAB A COFFE FOR YOURSELF TILL THE TIME SCAN GETS COMPLETED...:)', 'blue', attrs=['blink'])
+        print termcolor.colored('\nGO...GRAB A COFFE FOR YOURSELF TILL THE TIME SCAN GETS COMPLETED..:)', 'blue', attrs=['blink'])
 
         try:
-            with open(target_list,"r") as wordlist:
+            with open(options.wordlist,"r") as wordlist:
                 for line in wordlist:
                     try:
                         word = line.strip()
-                        url = word + "." + target_url
+                        url = word + "." + options.domain
                         f = requests.get("http://" + url)
-                        print termcolor.colored("\nSubdomain Found: {}".format(url), 'green', attrs=['bold'])
+                        print termcolor.colored('\nSubdomain Found: {}'.format(url), 'green', attrs=['bold'])
             
                     except requests.exceptions.ConnectionError:
                         pass
 
         except KeyboardInterrupt:
-            print termcolor.colored('\n[+]Ctrl + c..Byeeee...', 'red')
+            print('\n[+]Ctrl + c..Byeeee...')
 
 except KeyboardInterrupt:
-    print termcolor.colored('\nByeeeee....', 'red')
+    print('\nByeeeee....')
